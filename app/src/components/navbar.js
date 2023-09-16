bf300af14c007c0e00da94e2e4bcd8287727f569
mofopeadegoke <mofopeadegoke@gmail.com>
2023-09-16 19:00:38 +0300

    Most fixes made

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -1,49 +1,64 @@
 import LocationIcon from "../assets/locationIcon.png";
-// import burger from "../assets/burger.svg";
+import burger from "../assets/burger.svg";
 import Button from "./button.js";
 import "./navbar.css";
 import { Link } from "react-router-dom";
-// // Function that handles the responsive navbar hamburger
-// function navBurger() {
-//   const navHideEls = document.querySelectorAll(".navHide");
-//   const menuLinkEls = document.querySelectorAll(".menuLink");
-//   menuLinkEls.forEach((el) => {
-//     el.classList.toggle("navHide");
-//   });
-// }
-
-// // Function that handles the displaying of the dropdown menus
-// function handleClick(value) {
-//   const programDropdownMenu = document.querySelector(`.dropdownMenu.${value}`);
-//   programDropdownMenu.classList.toggle("show");
-//   window.onclick = function (e) {
-//     if (!e.target.matches(`.${value}`)) {
-//       if (programDropdownMenu.classList.contains("show")) {
-//         programDropdownMenu.classList.remove("show");
-//       }
-//     }
-//     // if (!e.target.matches(`.${value} + img`)) {
-//     //   if (programDropdownMenu.classList.contains("show")) {
-//     //     programDropdownMenu.classList.remove("show");
-//     //   }
-//     // }
-//   };
-// }
+import { useEffect } from "react";
+// Function that handles the responsive navbar hamburger
+function navBurger() {
+  const navHideEls = document.querySelector(".navbarSecondPart");
+  navHideEls.classList.toggle("hide");
+  console.log(navHideEls);
+}
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="#" className="navbarLogoArticle">
-        <p>Godaesil Ride</p>
-        <img src={LocationIcon} alt="A map marker" />
-      </Link>
-      <article className="navbarSecondPart">
-        <a href="#about">About Us</a>
-        <a href="#features">Features</a>
-        <a href="#privacy">Privacy</a>
-        <a href="#contact">
+    <>
+      <nav>
+        <Link to="#" className="navbarLogoArticle">
+          <p>Godaesil Ride</p>
+          <img src={LocationIcon} alt="A map marker" />
+        </Link>
+        <img
+          src={burger}
+          alt="Nav burger icon"
+          className="mobile"
+          onClick={navBurger}
+        />
+        <a href="#about" className="mobileHide">
+          About Us
+        </a>
+        <a href="#features" className="mobileHide">
+          Features
+        </a>
+        <a href="#privacy" className="mobileHide">
+          Privacy
+        </a>
+        <a href="#contact" className="mobileHide">
           <Button value="Join Waitlist"></Button>
         </a>
+      </nav>
+      <article className="navbarSecondPart hide">
+        <article className="header">
+          <Link to="#" className="navbarLogoArticle">
+            <p>Godaesil Ride</p>
+            <img src={LocationIcon} alt="A map marker" />
+          </Link>
+          <img
+            src={burger}
+            alt="Nav burger icon"
+            className="mobile"
+            onClick={navBurger}
+          />
+        </article>
+        <article className="links">
+          <a href="#about">About Us</a>
+          <a href="#features">Features</a>
+          <a href="#privacy">Privacy</a>
+          <a href="#contact">
+            <Button value="Join Waitlist"></Button>
+          </a>
+        </article>
       </article>
-    </nav>
+    </>
   );
 }
