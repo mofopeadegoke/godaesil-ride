4e6676875b8df788e6b65c88b8d200dc1ed80b2b
Adegoke Mofopefoluwa Daniel <110726038+mofopeadegoke@users.noreply.github.com>
2025-10-07 16:45:56 +0300

    footer improved

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = React.memo(() => {
   return (
     <>
       <footer className="footer" id="privacy">
-        <p>&copy;2023 Godaesil All Rights Reserved</p>
+        <p>&copy; {new Date().getFullYear()} Godaesil All Rights Reserved</p>
       </footer>
     </>
   );
